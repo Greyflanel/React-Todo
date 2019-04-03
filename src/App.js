06672fc84bfe33c9690cdcc21fb905e9ce42bc47
Greyflanel <richardlovelace811@gmail.com>
2019-04-03 17:50:53 -0500

    My first commit. Just going off of the guided project and other assistance. I am not sure of how everything works yet, but I am working on it.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import React from 'react';
+import TodoForm from './TodoForm.js';
+
+
+
+const todos = [ {
+task: 'Organize Garage',
+id: Date.now,
+completed: false
+},
+{
+  task:'Bake Cookies',
+  id: Date.now,
+  completed: false
+},
+{
+  task: 'Clean House',
+  id: Date.now,
+  completed: false
+}];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toDoItems: todos,
+      task: "",
+      id: Date.now,
+      completed: false
+    };
+  }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      toDoItems: [
+        ...this.state.toDoItems,
+        {
+          task: this.state.task,
+          id: Date.now(),
+          completed: false 
+        }
+      ],
+
+     task: "" 
+    });
+  }
+    
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,9 +58,11 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <p>My parentheses</p>
       </div>
     );
+    }
   }
-}
+ 
 
 export default App;
